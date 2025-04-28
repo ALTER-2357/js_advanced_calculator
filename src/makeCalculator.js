596a243a551026c3ba104612928c8a86f1ecd4f6
lewis_mills <lewismills290@protonmail.com>
2025-04-28 09:37:10 +0100

    develop

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,36 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  let result = 0;
+
+  const calculator = {
+    result: 0,
+
+    add: (value) => result + value,
+    subtract: (value) => result - value,
+    multiply: (value) => result * value,
+    divide: (value) => {
+      if (value === 0) {
+        throw new Error("Cannot divide by zero");
+      }
+      return result / value;
+    },
+
+    operate: function (callback, value) {
+      result = callback(value);
+      this.result = result;
+      return this; // Enable method chaining
+    },
+
+    reset: function () {
+      result = 0;
+      this.result = result;
+      return this; // Enable method chaining
+    }
+  };
+
+  return calculator;
 }
+
 
 module.exports = makeCalculator;
